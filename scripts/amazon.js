1481fb9179b5e8215e2bc9af918de3d7cc12c481
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2023-09-10 14:46:55 +0400

    interactive cart icon

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-    image: 'images/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    image: 'images/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image: 'images/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
-
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -54,10 +28,57 @@ products.forEach((product) => {
       <option value="9">9</option>
       <option value="10">10</option>
     </select>
-    <button>Add to Cart</button>
+    <button class="add-to-cart-button button-primary js-add-to-cart"
+    data-product-id='${product.id}'>Add to Cart</button>
   </div>
     `;
 });
 
 const itemsBox = document.querySelector('.items-box')
 itemsBox.innerHTML = productsHTML
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item
+      }
+    })
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      })
+    }
+
+    let cartQuantity = 0
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity
+    })
+
+    const totalCartQuantity = document.getElementById('cart-quantity')
+
+    console.log(cart)
+    console.log(cartQuantity)
+
+    totalCartQuantity.innerHTML = cartQuantity
+
+    if (totalCartQuantity.innerHTML > 9) {
+      totalCartQuantity.style.left = '25px'
+    }
+
+    if (totalCartQuantity.innerHTML > 99) {
+      totalCartQuantity.style.fontSize = '12px'
+      totalCartQuantity.style.top = '9px'
+      totalCartQuantity.style.left = '24px'
+    }
+  })
+})
